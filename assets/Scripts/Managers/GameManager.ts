590917630465050252e3231188fab97c92e72766
lilixiang <1547836606@qq.com>
2019-03-25 14:37:14 +0800

    1

--- assets/Scripts/Managers/GameManager.ts
+++ assets/Scripts/Managers/GameManager.ts
@@ -17,6 +17,9 @@ export default class GameManager extends cc.Component {
 		resource: false
 	};
 
+	/**
+	 * Myname
+	 */
 	onLoad() {
 		GameManager.$ = this;
 		whevent.on(Events.LOADED_CONFIG, this.onLoadedConfig, this);
